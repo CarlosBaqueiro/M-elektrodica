57cc9811ec63c8a0f8b12784605ab6bf41fee8f9
Carlos Baqueiro Basto <carlosbaqueirob@gmail.com>
2024-10-16 12:13:47 -0600

    Add .gitignore for .idea files and constants for physics
    Added a .gitignore file to exclude specific .idea directory files for better project configuration management. Additionally, created a constants.py file defining key physical constants to be used in calculations.

---
+++ source/constants.py
@@ -0,0 +1,12 @@
+"""
+M-elektrodica:
+                Constants file
+@author : Carlos Baqueiro Basto
+
+Created on Julio 2024
+"""
+
+k_B = 8.617333262145e-5	 # [eV/K] Boltzmann's constant
+h = 4.135667696e-15 #[eV s] Planck's constant
+F = 96485.3321233100184# [C/mol] Faraday's constant
+R = 8.314 # [J/mol/K] Ideal gas constant
